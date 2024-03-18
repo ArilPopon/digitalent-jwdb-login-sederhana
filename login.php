0d6d75ed8502ba2b3217d6bcc8dbe5e3423fac20
Aril Ponco Nugroho <81026715+ArilPopon@users.noreply.github.com>
2024-03-18 10:23:11 +0700

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $username = 'root';
 $password = '';
-$database = 'jwdb_login';
+$database = 'login';
 $conn = mysqli_connect($host, $username, $password, $database);
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
